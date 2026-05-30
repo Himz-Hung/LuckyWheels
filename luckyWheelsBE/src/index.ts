c72a8d9f6199a2f5d1b2738cfc8e2f6ef6857feb
Himz <hungitforwork@gmail.com>
2026-05-30 15:59:09 +0700

    Himz - Implement project BE env

--- luckyWheelsBE/src/index.ts
+++ luckyWheelsBE/src/index.ts
@@ -1,3 +1,5 @@
+import "./config/env";
+
 import express from "express";
 import cors from "cors";
 
@@ -5,12 +7,11 @@ import authRoutes from "./routes/authRoutes";
 import luckyItemRoute from "./routes/luckyItemRoutes";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-app.use("/api/lucky-item",luckyItemRoute);
+app.use("/api/lucky-item", luckyItemRoute);
 
 app.listen(3000, () => {
   console.log("Server running at port 3000");
